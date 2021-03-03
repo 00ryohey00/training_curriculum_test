db82ec4006f29c5597de5ebe24cd1dd4e2bc1428
00ryohey00 <77541490+00ryohey00@users.noreply.github.com>
2021-03-03 19:14:00 +0900

    Revert "Issue4~6の修正"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    #binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,8 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    #requireをcalendersからplanに変更
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   #↓getWeekを書き替え
@@ -38,12 +36,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = @todays_date.wday + x
-      if wday_num >= 7  #「wday_numが7以上の場合」という条件式
-       wday_num = wday_num -7
-      end
       #days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
